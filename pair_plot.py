14987522c675a79e9a0dcca0e62752501bacdb37
IKyllian <kyllian.delporte@gmail.com>
2025-01-29 20:51:52 +0100

    pair plot -> added colors for every house

--- pair_plot.py
+++ pair_plot.py
@@ -4,19 +4,27 @@ from utils import *
 from maths import *
 import numpy as np
 
+HOUSE = "Hogwarts House"
 def main():
     fileDataFrame = pandas.read_csv('./datasets/dataset_train.csv')
     dataFrame = fileDataFrame.drop(['Index'], axis='columns')
     dataFrame = dataFrame.dropna()
-    houses = splitHouse(dataFrame)
-    colors = ['red', 'blue', 'purple', 'green']
-    i = 0
-    finalDataFrame = []
-    for house in houses: 
-        houseDataFrame = house.select_dtypes(include=['number'])
-        houseDataFrame = standardizeDataFrame(houseDataFrame)
-        finalDataFrame.append(houseDataFrame)
-    g = pandas.plotting.scatter_matrix(pandas.concat(finalDataFrame), figsize=(15,15), marker = 'o', hist_kwds = {'bins': 20}, s = 1, alpha = 0.8, color=colors[0])
+    if HOUSE not in dataFrame.columns:
+        raise ValueError("La colonne 'House' n'existe pas dans le DataFrame.")
+    numeric_df = dataFrame.select_dtypes(include=['number'])
+    numeric_df = standardizeDataFrame(numeric_df)
+    numeric_df[HOUSE] = dataFrame[HOUSE]
+    houses = numeric_df[HOUSE].unique()
+    colors = {house: color for house, color in zip(houses, ['red', 'blue', 'purple', 'green'])}
+    pandas.plotting.scatter_matrix(
+        numeric_df.drop(HOUSE, axis=1),
+        figsize=(15, 15),
+        marker='o',
+        hist_kwds={'bins': 20},
+        s=1,
+        alpha=0.8,
+        c=numeric_df[HOUSE].map(colors),
+    )    
     plt.show()
 
 if (__name__ == "__main__"):
